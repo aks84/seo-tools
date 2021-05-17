3db4555dda5cfbee879d9c55c50970a72b72c757
Amulya Kumar Shahi <amulyaa_24@yahoo.in>
2021-05-17 17:38:09 +0530

    updated database for 2 models

--- lib/models/Blogging.dart
+++ lib/models/Blogging.dart
@@ -23,63 +23,63 @@ class Blogtool{
 List<Blogtool> blogList = [
 
 Blogtool(
-    name: "CMS",
+    name: "CMS - WordPress",
     logo: "images/wordpress.png",
     tooltype: "Blogging Tool",
-    description: "",
-    features: "",
-    plansnprice: "",
-    groupbuyprice: ""
+    description: "41% of the web is built on WordPress. No matter your site or your style, there’s a beautiful, pro layout waiting for you.",
+    features: "Free Open Source, Easy User Interface, Lightweight, Faster Page Load, Free Themes and Plugins, Theme Customizer, SEO Ready, eCommerce Ready.",
+    plansnprice: r"Personal: $5, Premium: $9, Business: $30",
+    groupbuyprice: r"cPanel: $1.60/Monthly"
   ),
 
 Blogtool(
     name: "StoryBase",
     logo: "images/storybase.png",
     tooltype: "Blogging Tool",
-    description: "",
-    features: "",
-    plansnprice: "",
-    groupbuyprice: ""
+    description: "Start Growing Your Organic Traffic Today. Stop guessing what your target audience is searching for, analyze and optimize content for growth.",
+    features: "Generate Content Ideas, Get Long Tail Keywords and Phrases, Stop the Guess Work, Write Content Fast.",
+    plansnprice: r"Professional: $49, Agency: $299, Premium: Sales Team",
+    groupbuyprice: r"Agency: $10"
   ),
 
 Blogtool(
     name: "StoryBlocks",
     logo: "images/storyblocks.png",
     tooltype: "Blogging Tool",
-    description: "",
-    features: "",
-    plansnprice: "",
-    groupbuyprice: ""
+    description: "Tell all your best stories. Meet the growing demand for video with subscription plans that give you unlimited stock downloads, flexible licensing, and an easy-to-use video editor.",
+    features: "Stock Video, Stock Audio, Stock Images and Video Maker.",
+    plansnprice: r"Unlimited: $29, Enterprise: Sales Team",
+    groupbuyprice: r"$5"
   ),
 
 Blogtool(
     name: "Canva",
     logo: "images/canva.png",
     tooltype: "Blogging Tool",
-    description: "",
-    features: "",
-    plansnprice: "",
-    groupbuyprice: ""
+    description: "Design, Collaborate, Share. You can design, print and work in Canva, by yourself or as a team.",
+    features: "Image, Video, Animatoin, PPT, Templates for Social Media Pages, and more",
+    plansnprice: r"Free: $0, Pro: $10, Enterprise: $30",
+    groupbuyprice: r"Enterprise: $5"
   ),
 
 Blogtool(
     name: "BuzzSumo",
     logo: "images/buzzsumo.png",
     tooltype: "Blogging Tool",
-    description: "",
-    features: "",
-    plansnprice: "",
-    groupbuyprice: ""
+    description: "Find the content that performs best. Collaborate with the influencers who matter. Use our content insights to generate ideas, create high-performing content, monitor your performance and identify influencers.",
+    features: "Dicovery, Research, Find Influencers, Monitor User Engagement.",
+    plansnprice: r"Pro: $99, Plus: $179, Large:$299",
+    groupbuyprice: r"Pro: $5"
   ),
 
 Blogtool(
     name: "Grammarly",
     logo: "images/grammarly.png",
     tooltype: "Blogging Tool",
-    description: "",
-    features: "",
-    plansnprice: "",
-    groupbuyprice: ""
+    description: "Go beyond grammar. Choose a plan to ensure that everything you write is clear, engaging, and polished.",
+    features: "Grammar Checker, Punctuation, Word Choice, Fluency, Tone Detector, Plagiarism Checker.",
+    plansnprice: r"Premium: $12/Month, Business: $12.50/Memeber/Month. ",
+    groupbuyprice: r"$3/Month"
   ),
 
 ];
--- lib/models/Ecommerce.dart
+++ lib/models/Ecommerce.dart
@@ -26,70 +26,70 @@ List<EcomTool> ecomList = [
       name: "Ecom Inspector",
       logo: "images/ecominspector.png",
       tooltype: "eCommerce Tool",
-      description: "",
-      features: "",
-      plansnprice: "",
-      groupbuyprice: ""
+      description: "Competitive Intelligence So You Can Be Inspired. Track any Shopify store's product launches, best sellers, traffic, sales, ad campaigns, apps, and more.",
+      features: "Top Selling Products, Trending Shops, Recent Product Launches, Best Sellers, Store Technical Stack Insights.",
+      plansnprice: r"Start: $29, Grow: $59, Scale: $149, Conquer: $299",
+      groupbuyprice: r"Scale: $10"
   ),
 
   EcomTool(
       name: "Helium 10",
       logo: "images/helium10.png",
       tooltype: "eCommerce Tool",
-      description: "",
-      features: "",
-      plansnprice: "",
-      groupbuyprice: ""
+      description: "Everything You Need to Sell on Amazon and More. Easily manage and grow a thriving e-commerce business with just one platform.",
+      features: "Product Research, Keyword Research, Listing Optimization, Operations, Analytics, and Marketing.",
+      plansnprice: r"Starter: $37, Platinum: $97, Diamond: $197",
+      groupbuyprice: r"Diamond: $20"
   ),
 
   EcomTool(
       name: "Jungle Scout",
       logo: "images/junglescout.png",
       tooltype: "eCommerce Tool",
-      description: "",
-      features: "",
-      plansnprice: "",
-      groupbuyprice: ""
+      description: "Jungle Scout offers enterprise-level market insights and competitive analysis for brands looking to win the Amazon channel.",
+      features: "Product Tracker, Product Database, Supplier Database, Keyword Scout, Rank Tracker, Listing Builder, Academy, Analytics, Alerts.",
+      plansnprice: r"Basic: $29, Suite: $49, Professional: $84",
+      groupbuyprice: r"Professional: $10"
   ),
 
   EcomTool(
       name: "SaleHoo",
       logo: "images/salehoo.png",
       tooltype: "eCommerce Tool",
-      description: "",
-      features: "",
-      plansnprice: "",
-      groupbuyprice: ""
+      description: "SaleHoo is a powerful research tool, supplier directory, and online community for dropshippers, wholesalers, and e-commerce stores, with a thousands of products from all the market categories.",
+      features: "Supplier Directory, Market Research Labs, Members Forum, Email Support",
+      plansnprice: r"Directory: $69/Year, DropShip: $97/Month, Educate: $47/Course",
+      groupbuyprice: r"DropShip: $10"
   ),
 
   EcomTool(
       name: "AMZ Scout",
       logo: "images/amzscout.png",
       tooltype: "eCommerce Tool",
-      description: "",
-      features: "",
-      plansnprice: "",
-      groupbuyprice: ""
+      description: "Build Financial Freedom Selling on Amazon. Hot Products and Amazon Trend Reports. Step-by-Step 'How to sell on Amazon' Course.",
+      features: "Product Database, Pro Extension, Keyword Explorer, Keyword Tracker, Amazon Dropshipping, and Course.",
+      plansnprice: r"Toolset: $129/Quaterly, Toolset: $359/Yearly",
+      groupbuyprice: r"$10/Monthly"
   ),
 
   EcomTool(
       name: "Merchant Words",
       logo: "images/merchantwords.png",
       tooltype: "eCommerce Tool",
-      description: "",
-      features: "",
-      plansnprice: "",
-      groupbuyprice: ""
+      description: "Uncover product opportunities, spy on your competition, create successful marketing campaigns, and grow your sales and business with our tools and services.",
+      features: "Amazon Data, Digital Shelf, Market Insights, Keyword Collection, Bulk Search, ASIN Plus, Page 1 Products, Emerging Trends, Keyword Muliplier.",
+      plansnprice: r"Silver: $29, Gold: $79, Platinum: $149, ",
+      groupbuyprice: r"Platinum: $15"
   ),
 
   EcomTool(
       name: "Viral Launch",
       logo: "images/virallaunch.png",
       tooltype: "eCommerce Tool",
-      description: "",
-      features: "",
-      plansnprice: "",
-      groupbuyprice: ""
+      description: r"Viral Launch gives your brand the Amazon seller tools needed to scale. With our easy-to-use software suite (responsible for helping drive over $8 billion in sales on Amazon), you own the competitive advantage.",
+      features: "Amazon Product Research, Amazon Competitor Tracking, Amazon Keyword Research, Amazon PPC, Keyword Tracking.",
+      plansnprice: r"Essentials: $69, Pro: $99, Pro Plus Ads: $199",
+      groupbuyprice: r"Pro: $15"
   ),
 
 ];
